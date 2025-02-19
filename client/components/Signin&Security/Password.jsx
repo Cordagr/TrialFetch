e5fb079a93ee9e5c52fb58a43e96d3f4588ee03b
Cordagr <113870656+Cordagr@users.noreply.github.com>
2025-02-19 17:20:27 -0600

    Create Password.jsx
    Implementing password displaying

---
+++ client/components/Signin&Security/Password.jsx
@@ -0,0 +1,10 @@
+function Password() {
+	return (
+		<>
+			<div className="settingsSecurityPasswordTitle">Password</div>
+			<div className="settingsSecurityPassword">Password cannot be changed.</div>
+		</>
+	);
+}
+
+export default Password;
