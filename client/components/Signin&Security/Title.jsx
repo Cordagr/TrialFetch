e5e00c3bb244d939fe3be0d3712ceef1ea8b7aef
Cordagr <113870656+Cordagr@users.noreply.github.com>
2025-02-19 17:22:36 -0600

    Create Title.jsx
    implementing security page

---
+++ client/components/Signin&Security/Title.jsx
@@ -0,0 +1,14 @@
+function Title() {
+	return (
+		<>
+			<button className="cardTitle">
+				<div className="cardTitleImg">
+					<img src="/icons/security.svg" alt="profile icon" />
+				</div>
+				<h1>Sign in & Security</h1>
+			</button>
+		</>
+	);
+}
+
+export default Title;
