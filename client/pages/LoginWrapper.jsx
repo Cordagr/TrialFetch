87e8b0d98b5efb7fb3f7f371f365ddacde6287ab
Cordagr <113870656+Cordagr@users.noreply.github.com>
2025-02-19 18:30:51 -0600

    Create LoginWrapper.jsx
    Implementing login wrapper

---
+++ client/pages/LoginWrapper.jsx
@@ -0,0 +1,31 @@
+import { Navigate } from "react-router-dom";
+import PropTypes from "prop-types";
+import { useAtom } from "jotai";
+import { testAtom } from "../atoms/testAtom";
+import { useEffect, useState } from "react";
+
+export function LoginWrapper(props) {
+	const [user] = useAtom(testAtom);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		// we fetch local storage on mount
+		if (user !== undefined) {
+			setLoading(false); //user data is ready
+		}
+	}, [user]);
+
+	if (loading) {
+		return null;
+	}
+
+	if (!user) {
+		return <Navigate to="/login" />;
+	}
+
+	return props.children;
+}
+
+LoginWrapper.propTypes = {
+	children: PropTypes.node.isRequired,
+};
